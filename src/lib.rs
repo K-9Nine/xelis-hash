d6674551fec084fa200d2ae078fc145d77b571c4
K9 <99043728+K-9Nine@users.noreply.github.com>
2024-04-28 19:47:33 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -44,18 +44,13 @@ pub struct Bytes8Alignment([u8; 8]);
 
 #[derive(Debug, Clone)]
 pub struct Input {
-    data: Vec<Bytes8Alignment>,
+    data: [Bytes8Alignment; BYTES_ARRAY_INPUT / 8],
 }
 
 impl Default for Input {
     fn default() -> Self {
-        let mut n = BYTES_ARRAY_INPUT / 8;
-        if BYTES_ARRAY_INPUT % 8 != 0 {
-            n += 1;
-        }
-
         Self {
-            data: vec![Bytes8Alignment([0; 8]); n]
+            data: [Bytes8Alignment([0; 8]); BYTES_ARRAY_INPUT / 8],
         }
     }
 }
@@ -94,35 +89,29 @@ pub fn xelis_hash_scratch_pad(input: &mut [u8], scratch_pad: &mut ScratchPad) ->
     xelis_hash(input, scratch_pad.as_mut_slice())
 }
 
-#[inline(always)]
+#[inline]
 fn stage_1(int_input: &mut [u64; KECCAK_WORDS], scratch_pad: &mut [u64; MEMORY_SIZE], a: (usize, usize), b: (usize, usize)) {
     for i in a.0..=a.1 {
         keccakp(int_input);
 
         let mut rand_int: u64 = 0;
         for j in b.0..=b.1 {
-            let pair_idx = (j + 1) & (KECCAK_WORDS - 1); // Bitwise AND for modulo by power of 2
-            let pair_idx2 = (j + 2) & (KECCAK_WORDS - 1);
+            let pair_idx = (j + 1) % KECCAK_WORDS;
+            let pair_idx2 = (j + 2) % KECCAK_WORDS;
 
             let target_idx = i * KECCAK_WORDS + j;
             let a = int_input[j] ^ rand_int;
-
-            // Pre-calculate XOR result for potential SIMD opportunity
-            let xor = int_input[pair_idx] ^ int_input[pair_idx2];
-            
-            // Defined here
+            // Branching
             let left = int_input[pair_idx];
-            let right = int_input[pair_idx2]; 
-
-            // Conditional selection using table lookup (consider for larger KECCAK_WORDS)
-            let selection_table = [
-                left & right, // 0
-                !(left & right), // 1
-                !xor, // 2
-                xor, // 3
-            ];
-            let v = selection_table[(xor & 0x3) as usize];
-
+            let right = int_input[pair_idx2];
+            let xor = left ^ right;
+            let v = match xor & 0x3 {
+                0 => left & right,
+                1 => !(left & right),
+                2 => !xor,
+                3 => xor,
+                _ => unreachable!(),
+            };
             let b = a ^ v;
             rand_int = b;
             scratch_pad[target_idx] = b;
@@ -139,30 +128,36 @@ pub fn xelis_hash(input: &mut [u8], scratch_pad: &mut [u64; MEMORY_SIZE]) -> Res
         return Err(Error);
     }
 
-    let int_input: &mut [u64; KECCAK_WORDS] = bytemuck::try_from_bytes_mut(&mut input[0..BYTES_ARRAY_INPUT])
-    .map_err(|_| Error)?;
+    let int_input: &mut [u64; KECCAK_WORDS] = unsafe {
+        std::mem::transmute(&mut input[0..BYTES_ARRAY_INPUT])
+    };
 
     // stage 1
     stage_1(int_input, scratch_pad, (0, STAGE_1_MAX - 1), (0, KECCAK_WORDS - 1));
     stage_1(int_input, scratch_pad, (STAGE_1_MAX, STAGE_1_MAX), (0, 17));
 
     // stage 2
-    #[inline(always)]
-    fn stage_2(int_input: &mut [u64; KECCAK_WORDS], scratch_pad: &mut [u64; MEMORY_SIZE], a: (usize, usize), b: (usize, usize)) {
-        let mut small_pad: &mut [u32; MEMORY_SIZE * 2] = bytemuck::try_cast_slice_mut(scratch_pad)
-            .map_err(|_| Error)?
-            .try_into()
-            .map_err(|_| Error)?;
-        for j in 0..small_pad.len() / SLOT_LENGTH {
+    let mut slots: [u32; SLOT_LENGTH] = [0; SLOT_LENGTH];
+    // this is equal to MEMORY_SIZE, just in u32 format
+    let small_pad: &mut [u32; MEMORY_SIZE * 2] = bytemuck::try_cast_slice_mut(scratch_pad)
+        .map_err(|_| Error)?
+        .try_into()
+        .map_err(|_| Error)?;
+
+    slots.copy_from_slice(&small_pad[small_pad.len() - SLOT_LENGTH..]);
+
+    let mut indices: [u16; SLOT_LENGTH] = [0; SLOT_LENGTH];
+    for _ in 0..ITERS {
+        for chunk in small_pad.chunks_exact(SLOT_LENGTH) {
             // Initialize indices
             for k in 0..SLOT_LENGTH {
                 indices[k] = k as u16;
             }
 
             for slot_idx in (0..SLOT_LENGTH).rev() {
-                let index_in_indices = (small_pad[j * SLOT_LENGTH + slot_idx] % (slot_idx as u32 + 1)) as usize;
+                let index_in_indices = (chunk[slot_idx] % (slot_idx as u32 + 1)) as usize;
                 let index = indices[index_in_indices] as usize;
-                indices[index_in_indices] = indices[slot_idx];
+                std::mem::swap(&mut indices[index_in_indices], &mut indices[slot_idx]);
 
                 #[cfg(feature = "nightly")]
                 {
@@ -170,51 +165,39 @@ pub fn xelis_hash(input: &mut [u8], scratch_pad: &mut [u64; MEMORY_SIZE]) -> Res
 
                     for k in (0..SLOT_LENGTH).step_by(16) {
                         let slot_vector = u32x16::from_array(unsafe { read_unaligned(&slots[k] as *const u32 as *const [u32; 16]) });
-                        let values = u32x16::from_array(unsafe { read_unaligned(&small_pad[j * SLOT_LENGTH + k] as *const u32 as *const [u32; 16]) });
+                        let values = u32x16::from_array(unsafe { read_unaligned(&chunk[k] as *const u32 as *const [u32; 16]) });
 
                         let sign_mask = (slot_vector >> 31).simd_eq(u32x16::splat(0));
                         sum_buffer = sign_mask.select(sum_buffer + values, sum_buffer - values);
                     }
 
                     if slots[index] >> 31 == 0 {
-                        sum_buffer[index % 16] -= small_pad[j * SLOT_LENGTH + index];
+                        sum_buffer[index % 16] -= chunk[index];
                     } else {
-                        sum_buffer[index % 16] += small_pad[j * SLOT_LENGTH + index];
+                        sum_buffer[index % 16] += chunk[index];
                     }
-
 
                     slots[index] += sum_buffer.reduce_sum();
                 }
-                
-                #[cfg(not(feature = "nightly"))]
-                  {
-                    let mut sum = slots[index];
-                    let offset = j * SLOT_LENGTH;
-                    for k in 0..index {
-                      let pad = small_pad[offset + k];
-                      sum = if slots[k] >> 31 == 0 {
-                        sum.wrapping_add(pad)
-                      } else {
-                            sum.wrapping_sub(pad)
-                      };
-                    }
 
-                    // Prefetch data for the next loop iteration
-                    unsafe {
-                      // Assuming memoffset::prefetchable takes a slice and a size as arguments
-                      memoffset::prefetchable(
-                        &small_pad[(offset + index + 1) % (MEMORY_SIZE * 2) as usize.. (offset + index + 16) % (MEMORY_SIZE * 2) as usize],
-                        16 // Adjust the size argument if the prefetch function requires it
-                      );
+                #[cfg(not(feature = "nightly"))]
+                {
+                    let mut sum = slots[index];
+                    for k in 0..index {
+                        let pad = chunk[k];
+                        sum = if slots[k] >> 31 == 0 {
+                            sum.wrapping_add(pad)
+                        } else {
+                            sum.wrapping_sub(pad)
+                        };
                     }
-    
                     for k in (index + 1)..SLOT_LENGTH {
-                      let pad = small_pad[offset + k];
-                      sum = if slots[k] >> 31 == 0 {
-                        sum.wrapping_add(pad)
-                      } else {
-                        sum.wrapping_sub(pad)
-                      };
+                        let pad = chunk[k];
+                        sum = if slots[k] >> 31 == 0 {
+                            sum.wrapping_add(pad)
+                        } else {
+                            sum.wrapping_sub(pad)
+                        };
                     }
 
                     slots[index] = sum;
@@ -235,10 +218,8 @@ pub fn xelis_hash(input: &mut [u8], scratch_pad: &mut [u64; MEMORY_SIZE]) -> Res
     let mut mem_buffer_a: [u64; BUFFER_SIZE] = [0; BUFFER_SIZE];
     let mut mem_buffer_b: [u64; BUFFER_SIZE] = [0; BUFFER_SIZE];
 
-    for i in 0..BUFFER_SIZE as u64 {
-        mem_buffer_a[i as usize] = scratch_pad[((addr_a + i) % MEMORY_SIZE as u64) as usize];
-        mem_buffer_b[i as usize] = scratch_pad[((addr_b + i) % MEMORY_SIZE as u64) as usize];
-    }
+    mem_buffer_a.copy_from_slice(&scratch_pad[addr_a as usize % MEMORY_SIZE..][..BUFFER_SIZE]);
+    mem_buffer_b.copy_from_slice(&scratch_pad[addr_b as usize % MEMORY_SIZE..][..BUFFER_SIZE]);
 
     let mut final_result = [0; HASH_SIZE];
 
@@ -256,10 +237,7 @@ pub fn xelis_hash(input: &mut [u8], scratch_pad: &mut [u64; MEMORY_SIZE]) -> Res
 
         let mut result = !(hash1 ^ hash2);
 
-        for j in 0..HASH_SIZE {
-            let a = mem_buffer_a[(j + i) % BUFFER_SIZE];
-            let b = mem_buffer_b[(j + i) % BUFFER_SIZE];
-
+        for (j, (&a, &b)) in mem_buffer_a.iter().zip(&mem_buffer_b).enumerate().take(HASH_SIZE) {
             // more branching
             let v = match (result >> (j * 2)) & 0xf {
                 0 => result.rotate_left(j as u32) ^ b,
@@ -293,7 +271,8 @@ pub fn xelis_hash(input: &mut [u8], scratch_pad: &mut [u64; MEMORY_SIZE]) -> Res
 
         let index = SCRATCHPAD_ITERS - i - 1;
         if index < 4 {
-            final_result[index * 8..(SCRATCHPAD_ITERS - i) * 8].copy_from_slice(&result.to_be_bytes());
+            let (left, _) = final_result.split_at_mut(index * 8);
+            left.copy_from_slice(&result.to_be_bytes()[..(SCRATCHPAD_ITERS - i) * 8]);
         }
     }
 
@@ -353,20 +332,4 @@ mod tests {
             106, 106, 173, 8, 207, 59, 118, 108, 176, 196, 9, 124, 250, 195, 3,
             61, 30, 146, 238, 182, 88, 83, 115, 81, 139, 56, 3, 28, 176, 86, 68, 21
         ];
-        test_input(&mut input, expected_hash);
-    }
-
-    #[test]
-    fn test_scratch_pad() {
-        let mut scratch_pad = ScratchPad::default();
-        let mut input = Input::default();
-
-        let hash = xelis_hash_scratch_pad(input.as_mut_slice().unwrap(), &mut scratch_pad).unwrap();
-        let expected_hash = [
-            0x0e, 0xbb, 0xbd, 0x8a, 0x31, 0xed, 0xad, 0xfe, 0x09, 0x8f, 0x2d, 0x77, 0x0d, 0x84,
-            0xb7, 0x19, 0x58, 0x86, 0x75, 0xab, 0x88, 0xa0, 0xa1, 0x70, 0x67, 0xd0, 0x0a, 0x8f,
-            0x36, 0x18, 0x22, 0x65,
-        ];
-        assert_eq!(hash, expected_hash);
-    }
-}
+        test_input
